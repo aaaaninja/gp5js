4088e80de34dad1404d6896978e36f8e19e07f7d
aaaaninja <aaaaninja@aaaa.ninja>
2020-03-28 15:54:44 +0900

    fin

--- 01_P/P_1_0_01/sketch.js
+++ 01_P/P_1_0_01/sketch.js
@@ -42,7 +42,7 @@ function draw() {
   background(mouseY / 2, 100, 100);
 
   fill(360 - mouseY / 2, 100, 100);
-  rect(360, 360, mouseX + 1, mouseX + 1);
+  rect(360, 360, mouseY, mouseX + 1);
 }
 
 function keyPressed() {
