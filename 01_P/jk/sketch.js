cd661421ac2a81447ae891c5caed6b86eab55a26
aaaaninja <aaaaninja@aaaa.ninja>
2020-03-30 15:22:46 +0900

    :eyes: Review 11101

--- 01_P/jk/sketch.js
+++ 01_P/jk/sketch.js
@@ -1,17 +1,9 @@
 'use strict';
 
 function setup() {
-  createCanvas(720, 720)
-  colorMode(HSB, 720, 100, 100)
-
-  rectMode(CENTER)
 }
 
 function draw() {
-  background(mouseX, 100, 100)
-
-  fill(720 - mouseX, 100, 100)
-  rect(360, 360, mouseY, mouseX)
 }
 
 function keyPressed() {
