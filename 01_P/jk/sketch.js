c349cdfea39de7beef1772b27c29943cc29ef54e
aaaaninja <aaaaninja@aaaa.ninja>
2020-03-30 16:23:08 +0900

    分散していた計算箇所を1箇所に

--- 01_P/jk/sketch.js
+++ 01_P/jk/sketch.js
@@ -11,15 +11,17 @@ function setup() {
 }
 
 function draw() {
-  for (let gridX = 0; gridX < width; gridX += mouseX + 2) {
+  const stepX = mouseX + 2
+
+  for (let gridX = 0; gridX < width; gridX += stepX) {
     fill(400, 100, 100)
-    rect(gridX, 0, mouseX + 2, 100)
+    rect(gridX, 0, stepX, 100)
 
     fill(500, 100, 100)
-    rect(gridX, 100, mouseX + 2, 100)
+    rect(gridX, 100, stepX, 100)
 
     fill(600, 100, 100)
-    rect(gridX, 200, mouseX + 2, 100)
+    rect(gridX, 200, stepX, 100)
   }
 }
 
